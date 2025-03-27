b18ab0d46d684c3496f25ab911b8bd40e446a209
Thiago Toma <thiagotoma99@gmail.com>
2025-03-27 15:02:16 -0300

    add swagger documentation

--- src/main/java/com/Thiago279/agendamento_medico/controller/ClientController.java
+++ src/main/java/com/Thiago279/agendamento_medico/controller/ClientController.java
@@ -5,6 +5,7 @@ import com.Thiago279.agendamento_medico.entity.Client;
 import com.Thiago279.agendamento_medico.entity.Schedule;
 import com.Thiago279.agendamento_medico.service.impl.ClientService;
 import com.Thiago279.agendamento_medico.service.impl.ScheduleService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,17 +22,20 @@ public class ClientController {
         this.service = service;
     }
 
+    @Operation(summary = "Retorna lista de todos os clientes", method = "GET")
     @GetMapping
     public ResponseEntity<List<Client>> listSchedules(){
         return ResponseEntity.ok(service.listAll());
     }
 
+    @Operation(summary = "Registra cliente (nome, email , numero de telefone)", method = "POST")
     @PostMapping
     public ResponseEntity<ClientRequestDTO> registerClient(@RequestBody ClientRequestDTO dto){
         service.registerClient(dto);
         return ResponseEntity.ok(dto);
     }
 
+    @Operation(summary = "Deleta cliente (id)", method = "DELETE")
     @DeleteMapping("{id}")
     public ResponseEntity<Object> deleteClient(@PathVariable Long id ){
         service.delete(id);
--- src/main/java/com/Thiago279/agendamento_medico/controller/ScheduleController.java
+++ src/main/java/com/Thiago279/agendamento_medico/controller/ScheduleController.java
@@ -5,6 +5,7 @@ import com.Thiago279.agendamento_medico.dto.ScheduleRequestDTO;
 import com.Thiago279.agendamento_medico.entity.Schedule;
 import com.Thiago279.agendamento_medico.service.impl.ClientService;
 import com.Thiago279.agendamento_medico.service.impl.ScheduleService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,11 +22,13 @@ public class ScheduleController {
         this.service = service;
     }
 
+    @Operation(summary = "Retorna lista de todos os agendamentos", method = "GET")
     @GetMapping
     public ResponseEntity<List<Schedule>> listSchedules(){
         return ResponseEntity.ok(service.listAll());
     }
 
+    @Operation(summary = "Cadastra agendamento de consulta", method = "POST")
     @PostMapping
     public ResponseEntity<Schedule> saveSchedule(@RequestBody @Valid ScheduleRequestDTO schedule){
         return ResponseEntity.ok(service.save(schedule));
